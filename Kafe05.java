22ef7b3b31612f98719ad53078d1a244360a3fe1
Alvido Wijaya <wijayaalvido@gmail.com>
2025-12-03 09:06:44 +0700

    Percobaan 1 jobsheet 12

---
+++ Kafe05.java
@@ -0,0 +1,18 @@
+public class Kafe05 {
+public static void Menu() {
+    System.out.println("===== MENU RESTO KAFE =====");
+    System.out.println("1. KOPI HITAM - Rp 15,000");
+    System.out.println("2. Cappucino - Rp 20,000");
+    System.out.println("3. Latte - Rp 22,000");
+    System.out.println("4. Teh Tarik - Rp 12,000");
+    System.out.println("5. Roti Bakar - Rp 10,000");
+    System.out.println("6. Mie Goreng - Rp 18,000");
+    System.out.println("===========================");
+    System.out.println("Silakan pilih menu yang Anda inginkan.");
+
+}
+public static void main(String[] args) {
+    Menu();
+    }
+    
+}
