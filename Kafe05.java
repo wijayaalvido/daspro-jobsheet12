5cf99f6e185452481f9b196766678d92f1bbe683
Alvido Wijaya <wijayaalvido@gmail.com>
2025-12-04 17:51:28 +0700

    nomer 6 percobaan 2

--- Kafe05.java
+++ Kafe05.java
@@ -1,7 +1,21 @@
 public class Kafe05 {
-public static void main(String[] args) {
+public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+    System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+    if (isMember) {
+        System.out.println("Anda adalah member, dapatkan diskon  10% untuk setiap pembelian!");
+    }
+
+    if (kodePromo.equals("DISKON50")) {
+        System.out.println("Promo: Diskon 50%");
+    } else if (kodePromo.equals("DISKON30")) {
+        System.out.println("Promo: Diskon 30%");
+    } else {
+        System.out.println("Kode promo tidak valid");
+    }
+
     System.out.println("===== MENU RESTO KAFE =====");
-    System.out.println("1. KOPI HITAM - Rp 15,000");
+    System.out.println("1. Kopi Hitam - Rp 15,000");
     System.out.println("2. Cappucino - Rp 20,000");
     System.out.println("3. Latte - Rp 22,000");
     System.out.println("4. Teh Tarik - Rp 12,000");
@@ -9,6 +23,8 @@ public static void main(String[] args) {
     System.out.println("6. Mie Goreng - Rp 18,000");
     System.out.println("===========================");
     System.out.println("Silakan pilih menu yang Anda inginkan.");
-
+    }
+    public static void main(String[] args) {
+    Menu("Andi", true, "DISKON50");
     }
 }
