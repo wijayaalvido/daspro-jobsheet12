4d86b052d609f4910d0c8d5e34f8459afed61467
Alvido Wijaya <wijayaalvido@gmail.com>
2025-12-05 10:33:55 +0700

    Tugas nomor 3 poin b jobsheet 12

--- RekapPenjualanCafe05.java
+++ RekapPenjualanCafe05.java
@@ -1,63 +1,112 @@
+import java.util.Scanner;
+
 public class RekapPenjualanCafe05 {
 
-    static String[] menu = {
-        "Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan"
-    };
+    static String[] menu;
+    static int[][] penjualan;
+    static int jumlahMenu, jumlahHari;
 
-    static int[][] penjualan = {
-        {20, 20, 25, 20, 10, 60, 10},   
-        {30, 80, 40, 10, 15, 20, 25},   
-        {5, 9, 20, 25, 10, 5, 45},   
-        {50, 8, 17, 18, 10, 30, 6},     
-        {15, 10, 16, 15, 10, 10, 55}    
-    };
+    static void inputData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah menu: ");
+        jumlahMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah hari penjualan: ");
+        jumlahHari = sc.nextInt();
+        sc.nextLine();
 
-    static void tampilkanTabel() {
-        System.out.printf("%-15s", "Menu");
-        for (int h = 1; h <= 7; h++) {
-            System.out.printf("Hari %d\t", h);
+        menu = new String[jumlahMenu];
+        penjualan = new int[jumlahMenu][jumlahHari];
+
+        System.out.println("\n-- Input Nama Menu --");
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print("Menu ke-" + (i+1) + ": ");
+            menu[i] = sc.nextLine();
         }
+
+        System.out.println("\n-- Input Penjualan per Hari (angka integer) --");
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.println("Penjualan '" + menu[i] + "':");
+            for (int j = 0; j < jumlahHari; j++) {
+                System.out.print("  Hari " + (j+1) + ": ");
+                penjualan[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    static void tampilkanTabelRapi() {
+        int firstColWidth = "Menu".length();
+        for (String m : menu) if (m.length() > firstColWidth) firstColWidth = m.length();
+        firstColWidth += 2; 
+
+        int[] colWidth = new int[jumlahHari];
+        for (int j = 0; j < jumlahHari; j++) {
+            String header = "Hari " + (j+1);
+            colWidth[j] = header.length();
+            for (int i = 0; i < jumlahMenu; i++) {
+                int len = String.valueOf(penjualan[i][j]).length();
+                if (len > colWidth[j]) colWidth[j] = len;
+            }
+            colWidth[j] += 2; 
+        }
+
+        int[] totalPerMenu = new int[jumlahMenu];
+        int maxTotalLen = "TOTAL".length();
+        for (int i = 0; i < jumlahMenu; i++) {
+            int s = 0;
+            for (int j = 0; j < jumlahHari; j++) s += penjualan[i][j];
+            totalPerMenu[i] = s;
+            int len = String.valueOf(s).length();
+            if (len > maxTotalLen) maxTotalLen = len;
+        }
+        maxTotalLen += 2;
+
+        System.out.printf("%-" + firstColWidth + "s", "Menu");
+        for (int j = 0; j < jumlahHari; j++) {
+            System.out.printf("%" + colWidth[j] + "s", "Hari " + (j+1));
+        }
+        System.out.printf("%" + maxTotalLen + "s", "TOTAL");
         System.out.println();
 
-        for (int i = 0; i < menu.length; i++) {
-            System.out.printf("%-15s", menu[i]);
-            for (int j = 0; j < 7; j++) {
-                System.out.printf("%d\t", penjualan[i][j]);
+        int totalTableWidth = firstColWidth;
+        for (int w : colWidth) totalTableWidth += w;
+        totalTableWidth += maxTotalLen;
+        for (int k = 0; k < totalTableWidth; k++) System.out.print("-");
+        System.out.println();
+
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.printf("%-" + firstColWidth + "s", menu[i]);
+            for (int j = 0; j < jumlahHari; j++) {
+                System.out.printf("%" + colWidth[j] + "d", penjualan[i][j]);
             }
+            System.out.printf("%" + maxTotalLen + "d", totalPerMenu[i]);
             System.out.println();
         }
-    }
 
-    static void menuTertinggi() {
-        int max = -1;
-        int index = 0;
+        for (int k = 0; k < totalTableWidth; k++) System.out.print("-");
+        System.out.println();
 
-        for (int i = 0; i < menu.length; i++) {
-            int total = 0;
-            for (int j = 0; j < 7; j++) total += penjualan[i][j];
-            if (total > max) {
-                max = total;
-                index = i;
-            }
+        System.out.printf("%-" + firstColWidth + "s", "TOTAL HARI");
+        int grandTotal = 0;
+        for (int j = 0; j < jumlahHari; j++) {
+            int colSum = 0;
+            for (int i = 0; i < jumlahMenu; i++) colSum += penjualan[i][j];
+            grandTotal += colSum;
+            System.out.printf("%" + colWidth[j] + "d", colSum);
         }
+        System.out.printf("%" + maxTotalLen + "d", grandTotal);
+        System.out.println();
 
-        System.out.println("\nMenu dengan total penjualan tertinggi:");
-        System.out.println(menu[index] + " = " + max);
-    }
-
-    static void rataRata() {
-        System.out.println("\nRata-rata penjualan setiap menu:");
-        for (int i = 0; i < menu.length; i++) {
-            int total = 0;
-            for (int j = 0; j < 7; j++) total += penjualan[i][j];
-            double avg = total / 7.0;
-            System.out.printf("%-15s: %.2f\n", menu[i], avg);
+        System.out.println();
+        System.out.println("Rata-rata penjualan tiap menu:");
+        for (int i = 0; i < jumlahMenu; i++) {
+            double avg = (double) totalPerMenu[i] / jumlahHari;
+            System.out.printf("%-" + firstColWidth + "s : %.2f%n", menu[i], avg);
         }
     }
 
     public static void main(String[] args) {
-        tampilkanTabel();
-        menuTertinggi();
-        rataRata();
+        inputData();
+        System.out.println();
+        tampilkanTabelRapi();
     }
 }
